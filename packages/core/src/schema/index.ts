2e8283879333cd9c98f6e29a5d4fb522ed1f4853
Hugo Marcelino <hugo.m.marcelino@gmail.com>
2023-11-18 22:21:23 +0000

    Revert changes about having a global SearchResult

--- packages/core/src/schema/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./hit"
-export * from "./hits-metadata"
-export * from "./search-result"
-export * from "./shard-failure"
-export * from "./shard-statistics"
-export * from "./total-hits"
