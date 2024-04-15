f73c628b9fcdadbd2c7f70e56ca34ec0b86c7324
Hugo Marcelino <hugo.m.marcelino@gmail.com>
2024-04-15 09:47:45 +0100

    reportm: 0.7.0

--- packages/reportm/src/schema/report-generation-request.ts
+++ packages/reportm/src/schema/report-generation-request.ts
@@ -22,6 +22,12 @@ export interface ReportGenerationRequest {
      */
     'arguments'?: { [key: string]: any; };
     /**
+     * A Map specifying the values to be extracted after the report has been generated. The keys represent the name of the extract and the value a cell address in the form of \"sheet!A1\".
+     * @type {{ [key: string]: string; }}
+     * @memberof ReportGenerationRequest
+     */
+    'extracts'?: { [key: string]: string; };
+    /**
      * 
      * @type {ReportGenerationRequestCallback}
      * @memberof ReportGenerationRequest
